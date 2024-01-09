9082aba99327bc15065e4b3bcac4ff4c92278e94
nkulisaturated <sobrietymedia@gmail.com>
2024-01-09 11:34:35 -0800

    Update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-"""Print the alphabet in lowercase, not followed by a new line."""
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    last_num = number % -10
+elif number >= 0:
+    last_num = number % 10
+if last_num > 5:
+    print(f"Last digit of {number} is {last_num} and is greater than 5")
+elif last_num == 0:
+    print(f"Last digit of {number} is {last_num} and is 0")
+else:
+    print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
 
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
