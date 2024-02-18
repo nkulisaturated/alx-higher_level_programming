77a02ba01c96d334dd422eec10e4acba09177138
nkulisaturated <sobrietymedia@gmail.com>
2024-02-18 06:20:49 -0800

    Update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
-"""
-This module defines `text_indentation`
-The function prints a text with 2 new lines after each of
-these characters: ., ? and :
-"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """splits a text into lines along "?", ":", "." followed by 2 new lines
-    Args:
-        text: input string
-    Returns:
-        No return
-    Raises:
-        TypeError: If text is not a string
-    """
+    """Print text with two new lines after each '.', '?', and ':'.
 
-    if type(text) is not str:
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    flag = 0
-    a = 0
-    while a < len(text):
-        if flag == 0:
-            if text[a] == ' ':
-                a += 1
-                continue
-            else:
-                flag = 1
-        if flag == 1:
-            if text[a] == '?' or text[a] == '.' or text[a] == ':':
-                print(text[a])
-                print()
-                flag = 0
-            else:
-                if text[a + 1] == '\n':
-                     a += 1
-                     continue 
-                print(text[a], end="")
-           a += 1
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
